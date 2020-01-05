875f04ace6a443bf5db2d4eb2e6e90addbdab687
mizdra <pp.mizdra@gmail.com>
2020-01-05 17:24:44 +0900

    UIフレームワークはデフォルトでは外しておく

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
-import { Button, Icon, Layout, Menu } from 'antd';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
-const { Footer, Content, Sider } = Layout;
+export type AppProps = {};
 
-export function App() {
+export function App(_props: AppProps) {
   const [count, setCount] = useState(0);
+  const handleClick = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
 
   return (
-    <Layout>
-      <Layout>
-        <Sider
-          style={{
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span className="nav-text">nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span className="nav-text">nav 2</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Content
-          style={{
-            backgroundColor: '#fff',
-            padding: 30,
-          }}
-        >
-          <Button onClick={() => setCount(count + 1)}>Count up: {count}</Button>
-        </Content>
-      </Layout>
-      <Footer>Footer</Footer>
-    </Layout>
+    <div>
+      <button onClick={handleClick}>{count}</button>
+    </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,5 @@
-import { LocaleProvider } from 'antd';
-import 'antd/dist/antd.css';
-import jaJP from 'antd/lib/locale-provider/ja_JP';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(
-  <LocaleProvider locale={jaJP}>
-    <App />
-  </LocaleProvider>,
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
